601d2236b3c0d6e9d74888ad8ca90c5c1ea6382b
wolfscatt <bjkeses11@hotmail.com>
2025-10-21 23:20:52 +0300

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // 👈 ÖNEMLİ: root domain (wolfscatt.com) için ekledik
+  base: "./", // 👈 ÖNEMLİ: root domain (wolfscatt.com) için ekledik
 
   server: {
     host: "::",
